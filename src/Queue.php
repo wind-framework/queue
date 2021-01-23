b2429aa99c6cc7f01ab113338dc69e40098a0e19
杨培 <ypnow@163.com>
2021-01-23 21:56:13 +0800

    Add Queue method comments.

--- src/Queue.php
+++ src/Queue.php
@@ -30,6 +30,16 @@ class Queue
         $this->connecting = $driver->connect();
     }
 
+	/**
+	 * Put job into queue
+	 *
+	 * @param Job $job The job to consume
+	 * @param int $delay Delay time seconds, 0 mean no delay.
+	 * @param int $priority The message priority, small number mean higher, big number mean lower.
+	 * For safety, use PRI_ prefix constant is recommend, some driver (like RedisDriver)
+	 * not support too much priority.
+	 * @return Promise<int> The Job id, you can delete the job use id before consume.
+	 */
     public function put(Job $job, $delay=0, $priority=self::PRI_NORMAL)
     {
         $message = new Message($job);
@@ -39,6 +49,14 @@ class Queue
         });
     }
 
+	/**
+	 * Delete the job
+	 *
+	 * The job can be delete only before job consume.
+	 *
+	 * @param int $id Job Id
+	 * @return Promise<bool>
+	 */
     public function delete($id)
     {
         return $this->call(function() use ($id) {
